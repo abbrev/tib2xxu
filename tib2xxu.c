49d0556bb7a0bfd7521f600a7812261d20c43fed
Christopher Williams <abbrev@gmail.com>
2014-07-26 17:18:39 -0700

    Display license information with usage

--- tib2xxu.c
+++ tib2xxu.c
@@ -74,12 +74,29 @@ static const char *argv0;
 static void usage()
 {
 	fprintf(stderr,
+"tib2xxu, Convert TIB file to XXU file\n"
+"\n"
+"Copyright 2014 Christopher Williams\n"
+"\n"
+"This program is free software: you can redistribute it and/or modify\n"
+"it under the terms of the GNU General Public License as published by\n"
+"the Free Software Foundation, either version 3 of the License, or\n"
+"(at your option) any later version.\n"
+"\n"
+"This program is distributed in the hope that it will be useful,\n"
+"but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
+"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"
+"GNU General Public License for more details.\n"
+"\n"
+"You should have received a copy of the GNU General Public License\n"
+"along with this program.  If not, see <http://www.gnu.org/licenses/>.\n"
+"\n"
 "Usage: %s [-t type] infile.tib outfile.??u\n"
 "       %s -h\n"
 "Options:\n"
 "       -t type  Calculator type (89u, 9xu, v2u). Output file extension is\n"
 "                used if this option is not given\n"
-"       -h       Display this message and exit\n", argv0, argv0);
+"       -h       Display this message and exit\n" , argv0, argv0);
 }
 
 static void die(const char *what, const char *msg)
